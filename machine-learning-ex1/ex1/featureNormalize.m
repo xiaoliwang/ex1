24e0d8e6c0f3344cf18f82cceac07b864d13b6a8
Tom <jiepengthegreat@126.com>
2016-12-21 12:03:57 +0800

    fix sumit function

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -6,10 +6,9 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   working with learning algorithms.
 
 % You need to set these values correctly
-X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
-
+mu = mean(X);
+sigma = std(X);
+X_norm = (X - mu) ./ sigma;
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
 %               of the feature and subtract it from the dataset,
